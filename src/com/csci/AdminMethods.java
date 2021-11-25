e32a8cd7a8253d47f4c5e5b18c0ed074514f7ef1
williw23 <62831828+williw23@users.noreply.github.com>
2021-11-25 11:12:12 +0800

    Update AdminMethods.java

--- src/com/csci/AdminMethods.java
+++ src/com/csci/AdminMethods.java
@@ -12,75 +12,102 @@ public class AdminMethods {
 
     public AdminMethods(Connection con) {
         this.con = con;
+        while(1){
+			int check = 0;
+			system.out.println()//menu
+
+			Scanner sc = new Scanner(System.in);
+			int choice = sc.nexInt();
+			switch(choice){
+                case 1:
+					AdminMethods am=new AdminMethods(con);
+       				break;
+                case 2:
+       				break;
+                case 3:
+       				break; 
+                case 4:
+                    break;
+                case 5:
+					check = 999;
+       			    break; 
+			}
+			if(check == 999)
+				break;
+		}
     }
 
     public void createTable(){
-//        CREATE TABLE user_category(
-//                ucid Integer,
-//                max_books Integer NOT NULL ,
-//                loan_period Integer NOT NULL ,
-//                PRIMARY KEY (ucid),
-//                CHECK (max_books<100 AND max_books>0),
-//                CHECK (loan_period<100 AND loan_period>0),
-//                CHECK (ucid<10 AND ucid > 0)
-//        );
-//        CREATE TABLE libuser(
-//                libuid VARCHAR(10),
-//                name VARCHAR(25) NOT NULL ,
-//                age INTEGER NOT NULL ,
-//                address VARCHAR(100) NOT NULL,
-//                ucid INTEGER,
-//                PRIMARY KEY (libuid),
-//                FOREIGN KEY (ucid) REFERENCES user_category(ucid),
-//                CHECK (AGE<1000 AND AGE>0)
-//        );/*three digit age?*/
-//
-//        CREATE TABLE book_category(
-//                bcid INTEGER,
-//                bcname VARCHAR(30) NOT NULL ,
-//                PRIMARY KEY (bcid),
-//                CHECK(bcid>0 AND bcid<10)
-//        );
-//
-//        CREATE TABLE book(
-//                callnum INTEGER,
-//                title VARCHAR(30) NOT NULL,
-//                publish DATE NOT NULL,
-//                rating FLOAT DEFAULT NULL,
-//                tborrowed INTEGER NOT NULL DEFAULT 0,
-//                bcid INTEGER NOT NULL,
-//                PRIMARY KEY (callnum),
-//                FOREIGN KEY (bcid) REFERENCES book_category(bcid),
-//                CHECK(tborrowed>-1 and tborrowed<100)
-//        );/*rating should be updated after returning book, rating can be null before first borrow!!!!!*/
-//        /*tborrow needs update with each borrow!!!!!!*/
-//        /*we can do these updates with an addition UPDATE statement*/
-//
-//        CREATE TABLE copy(
-//                callnum INTEGER,
-//                copynum INTEGER,
-//                PRIMARY KEY (callnum,copynum),
-//                FOREIGN KEY (callnum) REFERENCES book(callnum)
-//        );
-//        /*what is copynum*/
-//
-//        CREATE TABLE borrow(
-//                libuid VARCHAR(10),
-//                callnum INTEGER,
-//                copynum INTEGER,
-//                checkout DATE NOT NULL ,
-//                return_data DATE DEFAULT NULL,
-//                PRIMARY KEY (libuid,callnum,copynum,checkout),
-//                FOREIGN KEY (libuid) REFERENCES libuser(libuid),
-//                FOREIGN KEY (callnum,copynum) REFERENCES copy(callnum, copynum)
-//        );/*return can be null!!!!!*/
-//
-//        CREATE TABLE authorship(
-//                aname VARCHAR(25),
-//                callnum INTEGER,
-//                PRIMARY KEY (aname,callnum),
-//                FOREIGN KEY (callnum) REFERENCES book(callnum)
-//        );
+        // need to add some code to check success or fail.
+        Statement stmt = conn.createstatement();
+        String str = "CREATE TABLE user_category( " +
+            "ucid Integer, " +
+            "max_books Integer NOT NULL, " +
+            "loan_period Integer NOT NULL, " +
+            "PRIMARY KEY (ucid), " +
+            "CHECK (max_books<100 AND max_books>0), " +
+            "CHECK (loan_period<100 AND loan_period>0)," +
+            "CHECK (ucid<10 AND ucid > 0))";
+        stmt.executeUpdate(str);
+        
+        str = "CREATE TABLE libuser( " +
+            "libuid VARCHAR(10), " +
+            "name VARCHAR(25) NOT NULL, " +
+            "age INTEGER NOT NULL, " +
+            "address VARCHAR(100) NOT NULL, " +
+            "ucid INTEGER, " +
+            "PRIMARY KEY (libuid)," +
+            "FOREIGN KEY (ucid) REFERENCES user_category(ucid)," +
+            "CHECK (AGE<1000 AND AGE>0))";/*three digit age?*/
+        stmt.executeUpdate(str);
+        
+        str = "CREATE TABLE book_category( " +
+            "bcid INTEGER, " +
+            "bcname VARCHAR(30) NOT NULL, " +
+            "PRIMARY KEY (bcid)," +
+            "CHECK(bcid>0 AND bcid<10))";
+        stmt.executeUpdate(str);
+        
+        str = "CREATE TABLE book( " +
+            "callnum INTEGER, " +
+            "title VARCHAR(30) NOT NULL, " +
+            "publish DATE NOT NULL, " +
+            "rating FLOAT DEFAULT NULL, " +
+            "tborrowed INTEGER NOT NULL DEFAULT 0, " +
+            "bcid INTEGER NOT NULL, " +
+            "PRIMARY KEY (callnum), " +
+            "FOREIGN KEY (bcid) REFERENCES book_category(bcid), " +
+            "CHECK(tborrowed>-1 and tborrowed<100))";   /*rating should be updated after returning book, rating can be null before first borrow!!!!!*/
+                                                        /*tborrow needs update with each borrow!!!!!!*/
+                                                        /*we can do these updates with an addition UPDATE statement*/
+        stmt.executeUpdate(str);
+        
+        str = "CREATE TABLE copy( " +
+            "callnum INTEGER, " +
+            "copynum INTEGER, " +
+            "PRIMARY KEY (callnum,copynum), " +
+            "FOREIGN KEY (callnum) REFERENCES book(callnum))";  /*what is copynum*/
+        stmt.executeUpdate(str);
+        
+        str = "CREATE TABLE borrow( " +
+            "libuid VARCHAR(10), " +
+            "callnum INTEGER, " +
+            "copynum INTEGER, " +
+            "checkout DATE NOT NULL , " +
+            "return_data DATE DEFAULT NULL, " +
+            "PRIMARY KEY (libuid,callnum,copynum,checkout), " +
+            "FOREIGN KEY (libuid) REFERENCES libuser(libuid), " +
+            "FOREIGN KEY (callnum,copynum) REFERENCES copy(callnum, copynum))"; /*return can be null!!!!!*/
+        stmt.executeUpdate(str);
+        
+        str = "CREATE TABLE authorship( " +
+            "aname VARCHAR(25), " +
+            "callnum INTEGER, " +
+            "PRIMARY KEY (aname,callnum), " +
+            "FOREIGN KEY (callnum) REFERENCES book(callnum))" +;
+        stmt.executeUpdate(str);
+        
+        system.out.println("Processing... Done. Database is initialized.");
     }
 
     public void deleteAllTable() throws SQLException {
