448b9545372fcc4e7b1ea275a743cea1bef5a6c9
MY-Chris <76730997+MY-Chris@users.noreply.github.com>
2021-11-27 01:17:27 +0800

    complete search book methods of user

--- src/com/csci/UserMethods.java
+++ src/com/csci/UserMethods.java
@@ -1,32 +1,135 @@
 package com.csci;
 
-/*test*/
-
+import java.io.*;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class UserMethods {
 
-    Connection con;
+    private Connection con;
 
     public void userLoop(){
-
+        while(true){
+            System.out.println("-----Operations for user menu-----");
+            System.out.println("1. Search for books");
+            System.out.println("2. Show loan record of a user");
+            System.out.println("3. Return to the main menu");
+            System.out.print("Enter Your Choice:");
+            Scanner sc = new Scanner(System.in);
+            int choice = Integer.parseInt(sc.nextLine());
+            if(choice == 1){
+                System.out.println("Choose the Search criterion:");
+                System.out.println("1. call number");
+                System.out.println("2. title");
+                System.out.println("3. author");
+                System.out.print("Choose the Search criterion: ");
+                choice = Integer.parseInt(sc.nextLine());
+                System.out.print("Type in the Search Keyword: ");
+                String keyword = sc.nextLine();
+                System.out.println("|Call Num|Title|Book Category|Author|Rating|Available No. of Copy|");
+                if(choice == 1){
+                    try{
+                        searchForBookByCallNumber(keyword);
+                    }catch(Exception e){/*do nothing*/}
+                }
+                else if(choice == 2){
+                    try{
+                        searchForBookByTitle(keyword);
+                    }catch (Exception e){/*do nothing*/}
+                }
+                else if(choice == 3){
+                    try{
+                        searchForBookByAuthor(keyword);
+                    }catch(Exception e){/*do nothing*/}
+                }
+                else{
+                    System.out.println("Illegal input");
+                }
+            }
+            else if(choice == 2){
+                System.out.print("Enter The User ID: ");
+                String userID = sc.nextLine();
+                try{
+                    showBorrowRecord(userID);
+                }catch(Exception e){/*do nothing*/}
+            }
+            else if(choice == 3){
+                break;
+            }
+            else{
+                System.out.println("Illegal input!");
+                continue;
+            }
+        }
     }
     
 
-    public UserMethods(Connection con) {
+    public UserMethods(Connection con) throws SQLException{
         this.con = con;
     }
 
-    private void searchForBookByCallNumber(String callNumber){
-
+    private void searchForBookByCallNumber(String callNumber) throws SQLException{
+        String title = "";
+        String bcname = "";
+        String aname = "";
+        Float rating = null;
+        int numAvailableCopy = 0;
+        String sql = "SELECT title, rating, bcid FROM book WHERE callnum = ' " + callNumber + " '";
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(sql);
+        if(rset.next()){
+            title = rset.getString("title");
+            int bcid = rset.getInt("bcid");
+            rating = rset.getFloat("rating");
+            if(rset.wasNull()){
+                rating = null;
+            }
+            sql = "SELECT bcname FROM book_category WHERE bcid = ' " + String.valueOf(bcid) + " '";
+            rset = stmt.executeQuery(sql);
+            rset.next();
+            bcname = rset.getString("bcname");
+            sql = "SELECT aname FROM authorship WHERE callnum = ' " + callNumber + " '";
+            rset = stmt.executeQuery(sql);
+            rset.next();
+            aname = rset.getString("aname");
+            sql = "SELECT COUNT(*) FROM borrow WHERE callnum = ' " + String.valueOf(bcid) + " ' AND return_data = NULL";
+            rset = stmt.executeQuery(sql);
+            rset.next();
+            int numBorrowedCopy = rset.getInt(1);
+            sql = "SELECT COUNT(*) FROM copy WHERE callnum = ' " + String.valueOf(bcid) + " '";
+            rset = stmt.executeQuery(sql);
+            rset.next();
+            numAvailableCopy = rset.getInt(1) - numBorrowedCopy;
+            System.out.print("|" + callNumber + "|" + title + "|" + bcname + "|" + aname + "|");
+            if(rating == null)
+                System.out.println("null|" + String.valueOf(numAvailableCopy) + "|");
+            else
+                System.out.println(String.valueOf(rating) + "|" + String.valueOf(numAvailableCopy) + "|");
+        }
     }
-    private void searchForBookByTitle(String title){
 
+    private void searchForBookByTitle(String title) throws SQLException{
+        String sql = "SELECT callnum FROM book WHERE title = ' " + title + " '";
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(sql);
+        while(rset.next()){
+            searchForBookByCallNumber(rset.getString("callnum"));
+        }
     }
-    private void searchForBookByAuthor(String author){
 
+    private void searchForBookByAuthor(String author) throws SQLException{
+        String sql = "SELECT callnum FROM authorship WHERE aname = ' " + author + " '";
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(sql);
+        while(rset.next()){
+            searchForBookByCallNumber(rset.getString("callnum"));
+        }
     }
-    private void showBorrowRecord(String userID){
+
+    private void showBorrowRecord(String userID) throws SQLException{
 
     }
 
